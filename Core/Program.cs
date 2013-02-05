8fe26952726d449f0b9e633e25c85bbab991de61
Greg Burri <greg.burri@gmail.com>
2013-02-06 00:24:08 +0100

    First commit of the solution. Some template projects.

---
+++ Core/Program.cs
@@ -0,0 +1,26 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+#endregion
+
+namespace Gravity
+{
+   /// <summary>
+   /// The main class.
+   /// </summary>
+   public static class Program
+   {
+      private static Core core;
+
+      /// <summary>
+      /// The main entry point for the application.
+      /// </summary>
+      [STAThread]
+      static void Main()
+      {
+         core = new Core();
+         core.Run();
+      }
+   }
+}
